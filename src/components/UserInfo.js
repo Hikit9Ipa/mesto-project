21fab34dc8d789694c98293710ad7a387d81eac6
Hikit9Ipa <Hikit9ipa@gmail.com>
2022-07-02 21:36:55 +0300

    userInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,3 +2,25 @@
 // Принимает в конструктор объект с селекторами двух элементов: элемента имени пользователя и элемента информации о себе.
 // Содержит публичный метод getUserInfo, который возвращает объект с данными пользователя. Данные для этого метода нужно получать от методов класса Api — подумайте над тем, как внедрить метод класса Api в getUserInfo. Когда данные пользователя нужно будет подставить в форму при открытии — метод вам пригодится.
 // Содержит публичный метод setUserInfo, который принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу.
+
+import { data } from "jquery";
+
+export default class UserInfo{
+    constructor({userName,userInfo}){
+        this._userName = document.querySelector(userName);
+        this._userInfo = document.querySelector(userInfo);
+
+
+    }
+    getUserInfo(){
+
+        return {
+            userName: this._userName.textContent,
+            userInfo: this._userInfo.textContent
+        };
+    }
+    setUserInfo(username, userjob) {
+        this._userName.textContent = username;
+        this._userInfo.textContent = userinfo;
+      }
+}
